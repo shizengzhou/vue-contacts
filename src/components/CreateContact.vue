85627106e8e8ffda9266f79d85f1f98df90f4e02
szz <shizengzhou@outlook.com>
2019-03-13 23:30:05 +0800

    添加vue-router

---
+++ src/components/CreateContact.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>CreateContack</div>
+</template>
+
+<script>
+  export default {}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import ContactList from './components/ContactList.vue';
+import CreateContact from './components/CreateContact.vue';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: ContactList },
+    { path: '/create', component: CreateContact }
+  ]
+});
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
